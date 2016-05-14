063c85cd4b08ad3ce391c0139c860aa05706dddf
WilliamFisher <ashton0312@gmail.com>
2016-05-14 14:35:24 -0400

    Migration for bases

---
+++ database/migrations/2016_05_14_181739_create_bases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('imageurl')->default('img/thumbnaildefault.jpg')
+            $table->string('title');
+            $table->string('shortdescription', 140);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bases');
+    }
+}
