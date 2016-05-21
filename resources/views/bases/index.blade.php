77648ea7338d1ee130f6c7a840e2f57bc501ea2d
WilliamFisher <ashton0312@gmail.com>
2016-05-20 21:06:10 -0400

    Display base data in a table

--- resources/views/bases/index.blade.php
+++ resources/views/bases/index.blade.php
@@ -6,16 +6,27 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Browse Bases</div>
-
                 <div class="panel-body">
-                    @foreach ($bases as $base)
-                        <h4>{{ $base->title }}</h4>
-                        <ul>
-                            <li><img src={{ $base->imageurl }}></li>
-                            <li>{{ $base->shortdescription }}</li>
-                        </ul>
-                        <br>
-                    @endforeach
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>View Count</th>
+                                <th>Title</th>
+                                <th>User_ID</th>
+                                <th>Short Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($bases as $base)
+                            <tr>
+                                <td>{{ $base->view_count }}</td>
+                                <td>{{ $base->title }}</td>
+                                <td>{{ $base->user_id }}</td>
+                                <td>{{ $base->shortdescription }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
