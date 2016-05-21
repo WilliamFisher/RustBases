fe86437fe3c767d2e2938ea8ded30ab245769fd6
WilliamFisher <ashton0312@gmail.com>
2016-05-20 21:49:11 -0400

    Updated navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                    <li><a href="{{ url('/bases') }}">Browse Bases</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
