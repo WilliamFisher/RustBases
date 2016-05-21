f73c312843566da504781d5d30bfb864ee791c0f
WilliamFisher <ashton0312@gmail.com>
2016-05-20 20:27:41 -0400

    Added auth middleware to controller's constructor

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,6 +7,14 @@ use App\Base as Base;
 
 class BaseController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => [
+            'index',
+            'show',
+            ]]);
+    }
     /**
      * Display a listing of the resource.
      *
