84cc1568209556b83fb6437c787e9b20c90a6de6
WilliamFisher <ashton0312@gmail.com>
2016-07-24 14:14:02 -0400

    Fixed minor typo errors

--- resources/views/bases/index.blade.php
+++ resources/views/bases/index.blade.php
@@ -15,7 +15,7 @@
                             </a>
                             <div class="overlay">
                                 <p class="listing-name">{{ $base->title }}</p>
-                                <p class="listing-votes">{{ $base->view_count }} Votes</p>
+                                <p class="listing-votes">{{ $base->view_count }} Views</p>
                                 <a class="listing-author" href="#">{{ App\User::find($base->user_id)->name }}</a>
                                 <p class="listing-date">Created {{ Carbon\Carbon::now()->diffForHumans(Carbon\Carbon::parse($base->created_at), true) }} ago</p>
                             </div>
